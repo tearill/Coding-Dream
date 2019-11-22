b47cd87c74d7e5d3f94057ca8966a061deb3fba5
tearill <1105317666@qq.com>
2019-11-22 21:14:41 +0800

    sky

---
+++ html5/sky/readme.md
@@ -0,0 +1,6 @@
+- 页面特效+动作特效是一个特牛的领域
+  gasp.min.js
+  TweenMax.to(ele, 1, {
+      delay: 1,
+      ease: Back.easeOut,
+  })
---
+++ leetcode/rob_house/index.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    const len = nums.length;
+    if(len == 0)
+        return 0;
+    const dp = new Array(len + 1);
+    dp[0] = 0;
+    dp[1] = nums[0];
+    for(let i = 2; i <= len; i++) {
+        dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i-1]);
+    }
+    return dp[len];
+};
+console.log(rob([2,7,9,3,1]));
