d5862c1ccc77f3bb86104cdfe503195774a06900
tearill <1105317666@qq.com>
2019-11-26 19:23:54 +0800

    add new files

---
+++ miniprogram/yun_function/README.md
@@ -0,0 +1,5 @@
+- postman 模拟请求
+  不管界面
+  登录？
+  /login POST {username:'', password: ''} 
+  
---
+++ miniprogram/yun_function/miniprogram/pages/index/index.js
@@ -0,0 +1,14 @@
+//index.js
+Page({
+  http() {
+    // 要去其他网站拿数据
+    // 上云 计算能力特别强 前端，后端合作的
+    wx.cloud.callFunction({
+      name: 'http'
+    })
+    // 从云下来
+    .then(res => {
+      console.log(res);
+    })
+  }
+})
