6bf83586be39cecb1e3829efec11e7d97c824c9f
tearill <1105317666@qq.com>
2019-12-15 17:32:45 +0800

    车牌号识别

--- ai/car/main.js
+++ ai/car/main.js
@@ -4,7 +4,7 @@ const image = fs.readFileSync("cp.jpg").toString("base64");
 
 const AipOcrClient = require("baidu-aip-sdk").ocr;
 const client = new AipOcrClient('17712416', 'IwhS9PwnIOogKIQCT4MhIL4D', 'ZQCtcF306pEECXC7Sqe1XaYb4eSfqKAg');
-
+ 
 const options = {};
 options["multi_detect"] = "true";
 
