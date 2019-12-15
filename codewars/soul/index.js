d7bd6749797637782a63d0f7d256a18b62597570
tearill <1105317666@qq.com>
2019-12-15 17:34:38 +0800

    codewars题解

--- codewars/soul/index.js
+++ codewars/soul/index.js
@@ -7,6 +7,6 @@ const createPhoneNumber = (digits) => {
     // return `(${digits.splice(0,3).join('')})${digits.splice(0,3).join('')}-${digits.splice(0,4).join('')}`;
     return `(${digits.slice(0,3).join('')})${digits.slice(3,6).join('')}-${digits.slice(6,10).join('')}`;
 }
-
+ 
 console.log(createPhoneNumber([1,2,3,4,5,6,7,8,9,0]));
 // (123)456-7890
