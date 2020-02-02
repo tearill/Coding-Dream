3c59c442333feeb1b632ee283f2c3b60020cd11e
tearill <1105317666@qq.com>
2020-02-02 22:13:02 +0800

    添加新文件

--- mpvue-koa/netease-shop/src/pages/index/index.vue
+++ mpvue-koa/netease-shop/src/pages/index/index.vue
@@ -8,22 +8,33 @@
         <span class="icon"></span>
       </div>
     </div>
+    <div class="swiper">
+      <swiper class="swiper-container" indicator-dots="true" auto-play="true" interval="3000" circular="true" duration="500">
+        <block v-for="(item, index) in banner" :key="index">
+          <swiper-item class="swiper-item">
+            <image class="slide-image" :src="item.image_url" />
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
   </div>
 </template>
 
 <script>
-import amapFile from '../../utils/amap-wx'
-import { mapState, mapMutations } from 'vuex'
+import amapFile from "../../utils/amap-wx";
+import { mapState, mapMutations } from "vuex";
+import { get } from "../../utils"
 export default {
   data() {
     return {
+      banner: []
     };
   },
   computed: {
-    ...mapState(['cityName'])
+    ...mapState(["cityName"])
   },
   methods: {
-    ...mapMutations(['update']),
+    ...mapMutations(["update"]),
     toMappage() {
       // 通过wx.getSetting 先查询一下用户是否授权 "scoped.record"
       let _this = this;
@@ -35,7 +46,7 @@ export default {
             wx.openSetting({
               success: res => {
                 // 获取授权位置信息
-                _this.getCityName()
+                _this.getCityName();
               }
             });
           } else {
@@ -53,19 +64,25 @@ export default {
     },
     getCityName() {
       let _this = this;
-      var myAmapFun = new amapFile.AMapWX({key:'c509f492191fdc6f78adcd3894a5ad1c'});
+      var myAmapFun = new amapFile.AMapWX({
+        key: "c509f492191fdc6f78adcd3894a5ad1c"
+      });
       myAmapFun.getRegeo({
-        success: function (data) {
+        success: function(data) {
           // 成功回调
-          console.log(data, '---------');
+          console.log(data, "---------");
         },
-        fail: function (info) {
+        fail: function(info) {
           // 失败回调
-          console.log(info, '111111111');
+          console.log(info, "111111111");
           // _this.cityName = '北京'
-          _this.update({ cityName: '北京'})
+          _this.update({ cityName: "北京" });
         }
-      })
+      });
+    },
+    async getData() {
+      const data = await get('/index/index') // http://localhost:5757/lm
+      console.log(data)
     }
   }
 };
--- mpvue-koa/netease-shop/src/utils/index.js
+++ mpvue-koa/netease-shop/src/utils/index.js
@@ -17,6 +17,45 @@ export function formatTime (date) {
 
   return `${t1} ${t2}`
 }
+// 请求的封装
+const host = "http://localhost:5757/lm"
+export { host };
+
+// 请求封装
+function request (url, method, data, header = {}) {
+  wx.showLoading({
+    title: '加载中' //提示的内容
+  });
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: host + url, //开发者服务器接口地址",
+      data: data, //请求的参数",
+      method: method,
+      header: {
+        "content-type": "application/json"
+      },
+      success (res) {
+        wx.hideLoading();
+        resolve(res.data)
+      },
+      fail (error) {
+        wx.hideLoading();
+        reject(false)
+      },
+      complete () {
+        wx.hideLoading();
+      }
+    });
+  })
+}
+
+export function get (url, data) {
+  return request(url, 'GET', data)
+}
+
+export function get (url, data) {
+  return request(url, 'POST', data)
+}
 
 export default {
   formatNumber,
