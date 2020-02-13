6916de416102dadd098a5077558c14a08f03fa38
tearill <1105317666@qq.com>
2020-02-13 22:18:51 +0800

    完善商品详情页

--- mpvue-koa/netease-shop/src/pages/goods/index.vue
+++ mpvue-koa/netease-shop/src/pages/goods/index.vue
@@ -32,13 +32,100 @@
           <div></div>
       </div>
 
+      <!-- 商品参数 -->
+      <div class="attribute">
+          <div class="head">
+              商品参数
+          </div>
+          <div class="item" v-for="(item, index) in attribute" :key="index">
+              <div>{{item.name}}</div>
+              <div>{{item.value}}</div>
+          </div>
+      </div>
+
+      <!-- 图片展示 -->
+      <div class="detail" v-if="goods_desc">
+          <wxParse :content="goods_desc"></wxParse>
+      </div>
+
+      <!-- 常见问题 -->
+      <div class="common-problem">
+          <div class="h">
+              <text class="title">常见问题</text>
+          </div>
+          <div class="b">
+              <div class="item" v-for="(item, index) in issueList" :key="index">
+                  <div class="question-box">
+                      <text class="spot"></text>
+                      <text class="question">{{item.question}}</text>
+                  </div>
+                  <div class="answer">{{item.answer}}</div>
+              </div>
+          </div>
+      </div>
+
+      <!-- 大家都在看 -->
+      <div class="common-problem">
+          <div class="h">
+              <text class="title">大家都在看</text>
+          </div>
+          <div class="sublist">
+              <div v-for="(subitem, index) in productList" :key="index">
+                  <img :src="subitem.list_pic_url" alt="">
+                  <p>{{subitem.name}}</p>
+                  <p>¥{{subitem.retail_price}}</p>
+              </div>
+          </div>
+      </div>
+
+      <!-- footer -->
+      <div class="bottom-fixed">
+          <div class="collect-box" @click="collect">
+              <!-- 收藏 -->
+              <div class="collect" :class="[collectFlag ? 'active' : '']"></div>
+          </div>
+          <div class="car-box">
+              <!-- 购物车 -->
+              <div class="car" @click="toCart">
+                  <span>{{allnumber}}</span>
+                  <img src="../../../static/images/ic_menu_shoping_nor.png" alt="">
+              </div>
+          </div>
+          <div @click="buy">立即购买</div>
+          <div @click="addCart">加入购物车</div>
+      </div>
+
       <!-- 选择规格的弹出层 -->
-      
+      <div class="pop" v-show="showpop" @click="showType"></div>
+      <div class="attr-pop" :class="[showpop ? 'fadeup' : 'fadedown']">
+          <div class="top">
+              <div class="left">
+                  <img :src="info.primary_pic_url" alt="">
+              </div>
+              <div class="right">
+                  <div>
+                      <p>价格¥{{info.retail_price}}</p>
+                      <p>请选择数量</p>
+                  </div>
+              </div>
+              <div class="close" @click="showType">✖</div>
+          </div>
+          <div class="b">
+              <p>数量</p>
+              <div class="count">
+                  <div class="cut" @click="reduce">-</div>
+                  <input type="text" class="number" v-model="number" disabled="false">
+                  <div class="add" @click="add">+</div>
+              </div>
+          </div>
+      </div>
+
   </div>
 </template>
 
 <script>
-import { get, post } from '../../utils/index'
+import { get, post } from '../../utils';
+import wxParse from 'mpvue-wxparse';
 export default {
     data() {
         return {
@@ -47,8 +134,20 @@ export default {
             openId: '',
             info: {},
             brand: {},
-            showpop: false
+            showpop: false,
+            number: 0,
+            attribute: [],
+            goods_desc: '',
+            issueList: [], // 常见问题
+            productList: [], // 大家都在看
+            collectFlag: false, // 是否收藏
+            goods_id: '',
+            allnumber: 0, // 购物车商品数量
+            allPrice: ''
         }
+    },
+    components: {
+        wxParse
     },
     // 商品分享
     onShareAppMessage() {
@@ -71,14 +170,79 @@ export default {
             console.log(data);
             this.info = data.info;
             this.gallery = data.gallery;
+            this.attribute = data.attribute;
+            this.goods_desc = data.info.goods_desc;
+            this.issueList = data.issue;
+            this.productList = data.productList;
+            this.goods_id = data.info.id;
+            this.collectFlag = data.collected;
+            this.allnumber = data.allnumber;
+            this.allPrice = data.info.retail_price;
         },
         showType() {
             this.showpop = !this.showpop;
+        },
+        add() {
+            this.number += 1;
+        },
+        reduce() {
+            if (this.number > 1) {
+                this.number -= 1;
+            } else {
+                return false;
+            }
+        },
+        async collect() { // 收藏
+            this.collectFlag = !this.collectFlag;
+            const data = await post('/collect/addcollect', {
+                openId: this.openId,
+                goodsId: this.goods_id
+            });
+            console.log(data);
+        },
+        toCart() {
+            wx.switchTab({
+                url: '/pages/cart/main' 
+            });
+        },
+        async buy() {
+            if (this.showpop) {
+                if (this.number === 0) {
+                    wx.showToast({
+                        title: '请选择商品数量',
+                        duration: 2000,
+                        icon: 'none',
+                        mask: true,
+                        success: res => {}
+                    })
+                    return false;
+                }
+                const data = await post('/order/submitAction', {
+                    goods_id: this.goods_id,
+                    openId: this.openId,
+                    allPrice: this.allPrice
+                })
+                if (data) {
+                    wx.navigateTo({
+                        url: '/pages/order/main' 
+                    });
+                }
+            } else {
+                this.showpop = true;
+            }
+        },
+        addCart() {
+            if (this.showpop) {
+                
+            } else {
+                this.showpop = true;
+            }
         }
     },
 }
 </script>
 
 <style lang="less" scoped>
+@import url('~mpvue-wxparse/src/wxParse.css');
 @import './style.less';
 </style>
