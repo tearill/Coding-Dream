6ae8f41a5cb9656b059aac13e9470d72af5eeff0
tearill <1105317666@qq.com>
2020-02-13 22:23:31 +0800

    完善商品详情相关接口

--- mpvue-koa/netease-shop-node/controllers/goods/index.js
+++ mpvue-koa/netease-shop-node/controllers/goods/index.js
@@ -3,18 +3,63 @@ const { mysql } = require('../../mysql');
 // 商品详情页数据
 async function detailAction(ctx) {
     const goodsId = ctx.query.id;
-    console.log(goodsId)
+    const openId = ctx.query.openId
+
     // 商品信息
     const info = await mysql('nideshop_goods').where({
         'id': goodsId
     }).select();
+
     // 获取商品的图片
     const gallery = await mysql('nideshop_goods_gallery').where({
         'goods_id': goodsId
     }).select();
+
+    // 商品参数
+    // 关联查询两张表 leftJoin
+    const attribute = await mysql('nideshop_goods_attribute').column('nideshop_goods_attribute.value', 'nideshop_attribute.name')
+        .leftJoin('nideshop_attribute', 'nideshop_goods_attribute.attribute_id', 'nideshop_attribute.id')
+        .where({
+            'nideshop_goods_attribute.goods_id': goodsId
+        }).select();
+
+    // 常见问题
+    const issue = await mysql('nideshop_goods_issue').select();
+
+    // 大家都在看
+    const productList = await mysql('nideshop_goods').where({
+        'category_id': info[0].category_id
+    }).select();
+
+    // 判断是否收藏
+    const iscollected = await mysql('nideshop_collect').where({
+        'user_id': openId,
+        'value_id': goodsId
+    }).select();
+    let collected = false;
+    if (iscollected.length > 0) {
+        collected = true;
+    }
+    // 判断该用户的购物车里是否有此商品
+    const oldNumber = await mysql('nideshop_cart').where({
+        'user_id': openId
+    }).column('number').select();
+    let allnumber = 0;
+    if (oldNumber.length > 0) {
+        for (let i = 0; i < oldNumber.length; i++) {
+            const element = oldNumber[i];
+            allnumber += element.number;
+        }
+    }
+
     ctx.body = {
         'info': info[0] || [],
-        'gallery': gallery
+        'gallery': gallery,
+        'attribute': attribute,
+        'issue': issue,
+        'productList': productList,
+        'collected': collected,
+        'allnumber': allnumber
     }
 }
 
