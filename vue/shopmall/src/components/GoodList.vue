22bc21b868323a69dc65a69203ae0fffd32bbb20
tearill <1105317666@qq.com>
2020-02-28 17:54:55 +0800

    组件目录设计学习

---
+++ vue/shopmall/src/components/GoodList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    GoodsList
+    <img src="~~/logo.png" />
+  </div>
+</template>
+
+<script>
+// import '~/logo/png';
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/shopmall/src/components/GoodsDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- GoodsDetail -->
+    <!-- 商品信息展示 -->
+    <ShowGoods></ShowGoods>
+    <!-- 商品详细展示 -->
+    <ShowGoodsDetail></ShowGoodsDetail>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
