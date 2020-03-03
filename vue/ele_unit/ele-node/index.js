1616477d381945ca69c65b16d086dcc5e265f2ef
tearill <1105317666@qq.com>
2020-03-03 20:43:12 +0800

    项目上线学习

--- vue/ele_unit/ele-node/index.js
+++ vue/ele_unit/ele-node/index.js
@@ -2,7 +2,14 @@ const express = require('express')
 const app = express()
 const router = require('./routes/index.js')
 
+// api 服务
+const path = require('path')
 router(app)
+
+// web 前台 express.static 启用静态服务器
+app.use('/', express.static(path.join(__dirname, 'web')))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('/admin', express.static(path.join(__dirname, 'admin')))
 
 app.listen("3000", () => {
   console.log('api 服务器上线了')
