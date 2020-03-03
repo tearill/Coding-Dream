dc3b2e273ccddaab86eece14162303a26eb0fbe3
tearill <1105317666@qq.com>
2020-03-03 23:20:17 +0800

    vue3.0跨域+登录接口请求后台

--- vue/ele_unit/ele-admin/src/router/index.js
+++ vue/ele_unit/ele-admin/src/router/index.js
@@ -17,10 +17,19 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  { // 如果你在一定时间内登陆过，就去首页
+    // 如果你在任何页面，没有权限，后台一定会送你去登录页
+    path: '/login',
+    name: 'Login',
+    // 延迟加载
+    component: () => import('../views/Login.vue')
   }
 ]
 
 const router = new VueRouter({
+  // mode: 'history',
+  mode: 'hash',
   routes
 })
 
