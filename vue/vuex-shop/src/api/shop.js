8f8ff6d3891459785d544137aee679f661733461
tearill <1105317666@qq.com>
2020-03-18 21:45:51 +0800

    更新接口

--- vue/vuex-shop/src/api/shop.js
+++ vue/vuex-shop/src/api/shop.js
@@ -4,11 +4,28 @@ const _products = [
     title: 'iPad4 mini',
     price: 500.01,
     inventory: 2
+  },
+  { 
+    id: 2,
+    title: "H&M T-Shirt White",
+    price: 10.99,
+    inventory: 10
+  },
+  { 
+    id: 3,
+    title: "Charli XCX - Sucker CD",
+    price: 19.99,
+    inventory: 5
   }
 ]
 
 export default {
   getProducts(cb) {
     setTimeout(() => cb(_products), 1000)
+  },
+  buyProducts(products, cb, errorCb) {
+    setTimeout(() => {
+      (Math.random() > 0.5) ? cb() :errorCb()
+    }, 1000)
   }
 }
