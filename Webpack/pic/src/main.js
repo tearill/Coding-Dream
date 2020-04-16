981a82c81d79e641715eb9ae91772cb344ffd5c9
tearill <1105317666@qq.com>
2020-04-16 19:24:11 +0800

    webpack 处理图片、样式、字体

--- Webpack/pic/src/main.js
+++ Webpack/pic/src/main.js
@@ -2,3 +2,10 @@ import './styles/main.css'; // 依赖关系
 import './styles/main.styl'; // css 预处理
 import './styles/font.css'; // 字体
 console.log('Horace');
+
+// 使用相应的字体
+let body = document.querySelector('body');
+let h1 = document.createElement('h1', null, '外部引入字体');
+h1.innerText = '外部引入字体';
+h1.className = 'iconfont font';
+body.appendChild(h1);
