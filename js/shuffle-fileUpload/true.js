c51c773904651c648b5424efd53b8ccf5a715c3d
tearill <1105317666@qq.com>
2020-05-15 13:35:08 +0800

    洗牌算法

--- js/shuffle-fileUpload/true.js
+++ js/shuffle-fileUpload/true.js
@@ -5,14 +5,14 @@ let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 // 0
 // 每个数都参与交换
 function shuffle(arr) {
-  let len = arr.length
+  let len = arr.length;
   for (let i = 0; i < len; i++) {
-    console.log(Math.random() * (len - 1))
-    // let randomIndex = Math.floor(Math.random() * (len - i)) // 随机区间
+    console.log(Math.random() * (len - 1));
+    let randomIndex = Math.floor(Math.random() * (len - i)); // 随机区间
     // 从后往前选的最后一个数
-    // [arr[len - i - 1], arr[randomIndex]] = [arr[randomIndex], arr[len - i - 1]]
+    [arr[len - i - 1], arr[randomIndex]] = [arr[randomIndex], arr[len - i - 1]];
   }
-  return arr
+  return arr;
 }
 
 console.log(shuffle(arr))
